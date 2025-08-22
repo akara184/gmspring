14254b63be47661db7a2642e8efe0f8b5f62b89d
akara184 <davisuporterj@gmail.com>
2025-08-22 20:20:35 -0300

    Game seed

--- src/main/java/br/com/akara184/gmspring/entities/Game.java
+++ src/main/java/br/com/akara184/gmspring/entities/Game.java
@@ -15,13 +15,18 @@ public class Game {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String title;
-
+    
     @Column(name = "game_year")
     private Integer year;
     private String genre;
-    private String plataform;
+    private String platforms;
+    private Double score;
     private String imgUrl;
+
+    @Column(columnDefinition = "TEXT")
     private String shortDescription;
+
+    @Column(columnDefinition = "TEXT")
     private String longDescription;
 
     
@@ -29,82 +34,130 @@ public class Game {
         
     }
 
-    public Game(Long id, String title, Integer year, String genre, String plataform, String imgUrl,
+    
+    
+    public Game(Long id, String title, Integer year, String genre, String platforms, Double score, String imgUrl,
             String shortDescription, String longDescription) {
         this.id = id;
         this.title = title;
         this.year = year;
         this.genre = genre;
-        this.plataform = plataform;
+        this.platforms = platforms;
+        this.score = score;
         this.imgUrl = imgUrl;
         this.shortDescription = shortDescription;
         this.longDescription = longDescription;
     }
+
 
     public Long getId() {
         return id;
     }
 
+
+
     public void setId(Long id) {
         this.id = id;
     }
+
+
 
     public String getTitle() {
         return title;
     }
 
+
+
     public void setTitle(String title) {
         this.title = title;
     }
+
+
 
     public Integer getYear() {
         return year;
     }
 
+
+
     public void setYear(Integer year) {
         this.year = year;
     }
+
+
 
     public String getGenre() {
         return genre;
     }
 
+
+
     public void setGenre(String genre) {
         this.genre = genre;
     }
 
-    public String getPlataform() {
-        return plataform;
+
+
+    public String getPlatforms() {
+        return platforms;
     }
 
-    public void setPlataform(String plataform) {
-        this.plataform = plataform;
+
+
+    public void setPlatforms(String platforms) {
+        this.platforms = platforms;
     }
+
+
+
+    public Double getScore() {
+        return score;
+    }
+
+
+
+    public void setScore(Double score) {
+        this.score = score;
+    }
+
+
 
     public String getImgUrl() {
         return imgUrl;
     }
 
+
+
     public void setImgUrl(String imgUrl) {
         this.imgUrl = imgUrl;
     }
+
+
 
     public String getShortDescription() {
         return shortDescription;
     }
 
+
+
     public void setShortDescription(String shortDescription) {
         this.shortDescription = shortDescription;
     }
+
+
 
     public String getLongDescription() {
         return longDescription;
     }
 
+
+
     public void setLongDescription(String longDescription) {
         this.longDescription = longDescription;
     }
 
+                                  
+    
     @Override
     public int hashCode() {
         final int prime = 31;
@@ -130,15 +183,6 @@ public class Game {
             return false;
         return true;
     }
-
-                                  
     
-
-    
-
-    
-
-    
-    
-
+   
 }
