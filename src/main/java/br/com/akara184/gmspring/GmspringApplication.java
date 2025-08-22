64941d5c465269f4e94c6b53ccf28df8f0a94930
akara184 <davisuporterj@gmail.com>
2025-08-22 01:30:07 -0300

    first

---
+++ src/main/java/br/com/akara184/gmspring/GmspringApplication.java
@@ -0,0 +1,13 @@
+package br.com.akara184.gmspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GmspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GmspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/akara184/gmspring/GmspringApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.akara184.gmspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
